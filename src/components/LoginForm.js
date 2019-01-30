70f787ab5a91aea2357f8c871caffac714044f51
Eduardo <edubaik@gmail.com>
2019-01-29 17:34:59 -0800

    Create Input componenet

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
-    return <Card>
-        <CardSection />
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label='Email:'
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+        </CardSection>
         <CardSection />
         <CardSection>
           <Button>
             <Text>Log In</Text>
           </Button>
         </CardSection>
-      </Card>;
+      </Card>
+    );
   }
 }
